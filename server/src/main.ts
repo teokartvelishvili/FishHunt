c0a9fba22042f641554307315417bcad5215ff92
anano303 <124179773+anano303@users.noreply.github.com>
2025-02-13 21:36:08 +0400

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -54,7 +54,7 @@ async function bootstrap() {
       .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('docs', app, document);
   }
 
   app.enableShutdownHooks();
