fb323ab9fc3249281990e0f898083c452d40c84b
anano303 <124179773+anano303@users.noreply.github.com>
2025-02-13 14:46:02 +0400

    Update stripe.ts

--- web/src/lib/stripe.ts
+++ web/src/lib/stripe.ts
@@ -1,3 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+if (!process.env.STRIPE_SECRET_KEY) {
+  console.log("Stripe API key is missing, skipping Stripe payment process");
+  throw new Error("Stripe API key is not provided");
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
