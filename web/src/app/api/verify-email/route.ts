fa8cdd41c0732f3cc0d986a6685197eb80c13586
anano303 <124179773+anano303@users.noreply.github.com>
2025-02-18 17:59:10 +0400

    Update route.ts

--- web/src/app/api/verify-email/route.ts
+++ web/src/app/api/verify-email/route.ts
@@ -1,36 +1,43 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import { database } from "@/database/database";
 dotenv.config();
 
-// Verification code generation function
-const generateVerificationCode = () =>
-  Math.floor(100000 + Math.random() * 900000).toString();
-
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
   const { code, email } = await req.json();
 
   if (!email || !/\S+@\S+\.\S+/.test(email)) {
     console.log("Invalid email address:", req.body);
-    return new Response(JSON.stringify({ success: false, message: "Invalid email address" }), { status: 400, headers: { "Content-Type": "application/json" } });
+    return new Response(
+      JSON.stringify({ success: false, message: "Invalid email address" }),
+      { status: 400, headers: { "Content-Type": "application/json" } }
+    );
   }
 
   const verificationEntry = database.getEntry(email);
 
   if (!verificationEntry) {
-    return new Response(JSON.stringify({ success: false, message: "Email not found" }), { status: 404, headers: { "Content-Type": "application/json" } });
+    return new Response(
+      JSON.stringify({ success: false, message: "Email not found" }),
+      { status: 404, headers: { "Content-Type": "application/json" } }
+    );
   }
 
   if (verificationEntry.expiration < Date.now()) {
-    return new Response(JSON.stringify({ success: false, message: "Verification code expired" }), { status: 400, headers: { "Content-Type": "application/json" } });
+    return new Response(
+      JSON.stringify({ success: false, message: "Verification code expired" }),
+      { status: 400, headers: { "Content-Type": "application/json" } }
+    );
   }
 
   if (verificationEntry.code !== code) {
-    return new Response(JSON.stringify({ success: false, message: "Invalid verification code" }), { status: 400, headers: { "Content-Type": "application/json" } });
+    return new Response(
+      JSON.stringify({ success: false, message: "Invalid verification code" }),
+      { status: 400, headers: { "Content-Type": "application/json" } }
+    );
   }
 
-    return new Response(JSON.stringify({ success: true }), { status: 200, headers: { "Content-Type": "application/json" } });
+  return new Response(JSON.stringify({ success: true }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
