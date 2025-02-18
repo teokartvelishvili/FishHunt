72f27dd409b36e1dc74e930e326e7ee75d833c6c
anano303 <124179773+anano303@users.noreply.github.com>
2025-02-18 18:01:23 +0400

    .

--- web/src/database/database.ts
+++ web/src/database/database.ts
@@ -1,53 +1,54 @@
 import { readFileSync, writeFile } from "fs";
 import { VerificationEntry } from "./verification-entry";
 
-const dbFilePath = process.env.DB_FILE_PATH || 'db.json';
+const dbFilePath = process.env.DB_FILE_PATH || "db.json";
 
 class Database {
-    private static instance: Database;
-    private entries: VerificationEntry[] = [];
-    
-    private constructor() {
-        console.log('Database instance created');
-        this.fetchFromFS();
-    }
-    
-    public static getInstance(): Database {
-        if (!Database.instance) {
-            Database.instance = new Database();
-        }
-        return Database.instance;
-    }
+  private static instance: Database;
+  private entries: VerificationEntry[] = [];
 
-    public async addEntry(entry: VerificationEntry): Promise<void> {
-        this.entries.push(entry);
-        await this.writeFS();
-    }
+  private constructor() {
+    console.log("Database instance created");
+    this.fetchFromFS();
+  }
 
-    public getEntry(email: string): VerificationEntry | undefined {
-        return this.entries.findLast((entry) => entry.email === email);
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
     }
+    return Database.instance;
+  }
 
-    public clearDB(): void {
-        this.entries = [];
-        this.writeFS();
-    }
+  public async addEntry(entry: VerificationEntry): Promise<void> {
+    this.entries.push(entry);
+    await this.writeFS();
+  }
 
-    private fetchFromFS() {
-        try {
-            const data = readFileSync(dbFilePath, 'utf-8');
-            this.entries = JSON.parse(data);
-        } catch (error) {
-            this.clearDB();
-        }
-    }
+  public getEntry(email: string): VerificationEntry | undefined {
+    return this.entries.findLast((entry) => entry.email === email);
+  }
 
-    private writeFS() {
-        return new Promise((resolve) => {
-            const data = JSON.stringify(this.entries);
-            writeFile(dbFilePath, data, resolve);
-        });
+  public clearDB(): void {
+    this.entries = [];
+    this.writeFS();
+  }
+
+  private fetchFromFS() {
+    try {
+      const data = readFileSync(dbFilePath, "utf-8");
+      this.entries = JSON.parse(data);
+    } catch (error) {
+      console.log(error);
+      this.clearDB();
     }
+  }
+
+  private writeFS() {
+    return new Promise((resolve) => {
+      const data = JSON.stringify(this.entries);
+      writeFile(dbFilePath, data, resolve);
+    });
+  }
 }
 
 export const database = Database.getInstance();
