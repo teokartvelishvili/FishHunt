0538c5bb7653d104176e3e98552514687d5bd4a2
anano303 <124179773+anano303@users.noreply.github.com>
2025-02-18 17:57:04 +0400

    .

--- web/src/app/api/send-verification/route.ts
+++ web/src/app/api/send-verification/route.ts
@@ -1,4 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import { database } from "@/database/database";
@@ -10,23 +9,24 @@ const generateVerificationCode = () =>
 
 const EXPIRATION = +(process.env.EXPIRATION || 60000);
 
-export async function POST(
-  req: Request
-) {
+export async function POST(req: Request) {
   const { email } = await req.json();
   console.log("EMAIL_USER:", process.env.EMAIL_USER); // Log email username
   console.log("EMAIL_PASS:", process.env.EMAIL_PASS); // Log email password
   // Validate email format
   if (!email || !/\S+@\S+\.\S+/.test(email)) {
     console.log("Invalid email address:", req.body);
-    return new Response(JSON.stringify({ success: false, message: "Invalid email address" }), { status: 400, headers: { "Content-Type": "application/json" } });
+    return new Response(
+      JSON.stringify({ success: false, message: "Invalid email address" }),
+      { status: 400, headers: { "Content-Type": "application/json" } }
+    );
   }
 
   const verificationCode = generateVerificationCode();
 
   // Nodemailer transporter setup
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER, // Your email (e.g., "your-email@gmail.com")
       pass: process.env.EMAIL_PASS, // Your email password or App password
@@ -35,7 +35,7 @@ export async function POST(
       rejectUnauthorized: false, // disable certificate validation (use with caution)
     },
   });
-  transporter.verify(function (error, success) {
+  transporter.verify(function (error) {
     if (error) {
       console.error("Transporter verification failed:", error);
     } else {
@@ -54,19 +54,36 @@ export async function POST(
   };
 
   try {
-    await database.addEntry({ email, code: verificationCode, expiration: Date.now() + EXPIRATION });
+    await database.addEntry({
+      email,
+      code: verificationCode,
+      expiration: Date.now() + EXPIRATION,
+    });
     // Send the email
     await transporter.sendMail(mailOptions);
     // Respond with a success message and verification code
-    return new Response(JSON.stringify({ success: true }), { status: 200, headers: { "Content-Type": "application/json" } });
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error: unknown) {
     // Log the error and return failure response
     if (error instanceof Error) {
       console.error("Error sending email:", error.message);
-      return new Response(JSON.stringify({ success: false, message: "Failed to send email", error: error.message, }), { status: 500, headers: { "Content-Type": "application/json" } });
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "Failed to send email",
+          error: error.message,
+        }),
+        { status: 500, headers: { "Content-Type": "application/json" } }
+      );
     } else {
       console.error("Unknown error:", error);
-      return new Response(JSON.stringify({ success: false, message: "Failed to send email", }), { status: 500, headers: { "Content-Type": "application/json" } });
+      return new Response(
+        JSON.stringify({ success: false, message: "Failed to send email" }),
+        { status: 500, headers: { "Content-Type": "application/json" } }
+      );
     }
   }
 }
