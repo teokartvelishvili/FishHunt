942924f62ae2a5eb90dff74c5db6e37fe33dc795
anano303 <124179773+anano303@users.noreply.github.com>
2025-02-20 02:00:18 +0400

    fh

--- web/src/components/homePageForum/homePageForum.tsx
+++ web/src/components/homePageForum/homePageForum.tsx
@@ -2,15 +2,17 @@ import ForumPage from "@/app/(pages)/forum/ForumPage";
 import Link from "next/link";
 import "./homePageForum.css";
 const HomePageForum = () => {
+  return (
+    <div className="homePageForum">
+      <h1 className="homePageForumH1">Forum</h1>
 
-    return (
-      <div className="homePageForum">
-        <h1 className="homePageForumH1">Forum</h1>
+      <ForumPage />
+      <Link href="/forum" className="forumPageLink">
+        {" "}
+        See More{" "}
+      </Link>
+    </div>
+  );
+};
 
-        <ForumPage limit={3} />
-        <Link href="/forum" className="forumPageLink"> See More </Link>
-      </div>
-    );
-  };
-  
-  export default HomePageForum;
+export default HomePageForum;
