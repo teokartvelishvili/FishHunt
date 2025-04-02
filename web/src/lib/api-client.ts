4eee913a7d07618ceddc6c962136cb38cae3cad0
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-02 17:12:41 +0400

    Update api-client.ts

--- web/src/lib/api-client.ts
+++ web/src/lib/api-client.ts
@@ -36,7 +36,7 @@ apiClient.interceptors.response.use(
       try {
         await apiClient.post('/auth/refresh');
         return apiClient(originalRequest);
-      } catch (e) {
+      } catch {
         window.location.href = '/login';
       }
     }
