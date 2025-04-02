6561ff08187bb0332810d5584425430d7a7df1a7
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-02 22:23:29 +0400

    Revert "."
    This reverts commit 31fe68d3c8e6b8633e30988b31719fdf9449dc79.

--- server/src/cookie-config.ts
+++ server/src/cookie-config.ts
@@ -7,18 +7,22 @@ export const cookieConfig = {
       path: '/',
       maxAge: 20 * 60 * 1000,
       expires: new Date(Date.now() + 20 * 60 * 1000),
-      domain: '.fishhunt1.onrender.com', // ფიქსირებული მნიშვნელობა
+      domain: process.env.NODE_ENV === 'production' 
+        ? process.env.COOKIE_DOMAIN 
+        : undefined,
     },
   },
   refresh: {
     options: {
       httpOnly: true,
-      secure: true,
+      secure: true, // Always true for iOS
       sameSite: 'none' as const,
       path: '/',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      domain: '.fishhunt1.onrender.com', // ფიქსირებული მნიშვნელობა
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      domain: process.env.NODE_ENV === 'production' 
+        ? process.env.COOKIE_DOMAIN 
+        : undefined, // Add your domain in production
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // explicit expires for iOS
     },
   },
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -21,24 +21,17 @@ async function bootstrap() {
   //   allowedHeaders: ['Content-Type', 'Authorization'],
   // }));
 
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || 'https://soul-art.vercel.app';
+  console.log('Allowed Origins:', allowedOrigins);
   app.enableCors({
-    origin: [
-      'https://www.fishhunt.ge',
-      'https://fishhunt.ge',
-      'http://localhost:3000'
-    ],
+    origin:
+      process.env.ALLOWED_ORIGINS?.split(',') || 'https://fishhunt.vercel.app',
+
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: [
-      'Content-Type', 
-      'Accept', 
-      'Authorization', 
-      'Origin',
-      'X-Requested-With',
-      'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Credentials',
-    ],
-    exposedHeaders: ['Set-Cookie'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'forum-id', 'file-id', 'product-id'],
+
+    optionsSuccessStatus: 204,
   });
 
   app.enableVersioning({
